f11d14f773e2ae0080cbc7ee706801dd2c31cbcf
junior-df <gibadf@yahoo.com>
2025-10-22 22:10:33 -0300

    alteracao

--- js/app.js
+++ js/app.js
@@ -1,23 +1,4 @@
 
-//*SelecionarCompra();
-
-/*
-function SelecionarCompra (){
-
-    let selecao = document.getElementById('produto');
-    let botao = selecao.querySelector('.botao-form botao-adicionar');
-    let variavel = '';
-    
-    selecao.onchange = function (){
-        variavel = this.value;
-        alert (variavel);
-    }
-
-    //let valor = selecao.options[selecao.selectedIndex].value;
-    //alert(valor);
-    
-}
-*/ 
 
 function adicionar(){
    // recuperar valores nome e produto, quantidade e valor
@@ -35,27 +16,3 @@ function adicionar(){
    //atualizar o valor total 
 }
 
-
-/*
-function alterarStatus(id){
-   
-   let gameClicado = document.getElementById(`game-${id}`);
-   let imagem = gameClicado.querySelector('.dashboard__item__img');
-   let botao = gameClicado.querySelector('.dashboard__item__button');
-   let Nomejogo = gameClicado.querySelector('.dashboard__item__name');
-   let resultado = confirm('Confirmar devolução?');
-   
-   if (imagem.classList.contains('dashboard__item__img--rented')){
-      imagem.classList.remove('dashboard__item__img--rented');
-      botao.classList.remove('dashboard__item__button--return');
-      botao.textContent = 'Alugar';
-   } else {
-      imagem.classList.add('dashboard__item__img--rented');
-      botao.classList.add('dashboard__item__button--return');
-      botao.textContent = 'Devolver';
-   }
-   
-   
-   
-} 
-*/
